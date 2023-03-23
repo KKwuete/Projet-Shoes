f8e5a4dbc1bd0cf2181551a96093e43ea40197c9
KKwuete <marjorie.paulette@gmail.com>
2023-03-23 18:56:25 +0100

    index margin btw elements OK

--- project_shoes/static/main_app/app.js
+++ project_shoes/static/main_app/app.js
@@ -1,17 +1,3 @@
-$(document).ready(function () {
-  $(".carouselWrapper").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-    prevArrow:
-      "C:UsersmarjoOneDriveBureauProjet-Shoesimgiconangle-double-left.png",
-    nextArrow:
-      "C:UsersmarjoOneDriveBureauProjet-Shoesimgiconangle-double-right.png",
-  });
-});
-
 const popoverTriggerList = document.querySelectorAll(
   '[data-bs-toggle="popover"]'
 );

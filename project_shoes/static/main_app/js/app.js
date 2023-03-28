be0e02355918bcda52ef01b1a24671e1b58427f2
KKwuete <marjorie.paulette@gmail.com>
2023-03-28 17:22:21 +0200

    personnalisation msg description notOK

--- project_shoes/static/main_app/js/app.js
+++ project_shoes/static/main_app/js/app.js
@@ -2,11 +2,19 @@
   document.getElementById("my-text").innerHTML = "Hello, world!";
 }*/
 
-function mOver(obj) {
-  document.getElementById("my-text").innerHTML =
+/*function mOver() {
+  document.getElementById("desc-text").innerHTML =
     "Dessiné main avec pochoir Seigaiha_1, modèle: AIR FORCE 1";
 }
 
-function mOut(obj) {
-  document.getElementById("my-text").innerHTML = "";
+function mOut() {
+  document.getElementById("desc-text").innerHTML = "";
+}*/
+
+function mOver() {
+  document.getElementById("desc-text").style.display = "block";
+}
+
+function mOut() {
+  document.getElementById("desc-text").style.display = "none";
 }

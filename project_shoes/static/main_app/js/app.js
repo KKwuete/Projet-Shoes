9ebee4d346684a1f59d4a9ab0942b447d25d9464
KKwuete <marjorie.paulette@gmail.com>
2023-03-28 02:26:02 +0200

    mise en place onmouse event OK

---
+++ project_shoes/static/main_app/js/app.js
@@ -0,0 +1,12 @@
+/*function displayMsg() {
+  document.getElementById("my-text").innerHTML = "Hello, world!";
+}*/
+
+function mOver(obj) {
+  document.getElementById("my-text").innerHTML =
+    "Dessiné main avec pochoir Seigaiha_1, modèle: AIR FORCE 1";
+}
+
+function mOut(obj) {
+  document.getElementById("my-text").innerHTML = "";
+}
